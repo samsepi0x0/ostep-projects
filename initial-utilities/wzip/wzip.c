01b02dc7834b438b40839285342e9c039182601a
dumbermore <samsepi0x0@gmail.com>
2025-10-06 05:41:37 +0530

    Added WZIP utility function
    WZIP can accept multiple files in input, and will return a combined RLE for all the files present in the input
    For example:
    inputfile: aaa
    Run: ./wzip inputfile inputfile
    Output: 6a
    
    The characters from previous file are matched against the characters of the next file in sequence.

---
+++ initial-utilities/wzip/wzip.c
@@ -0,0 +1,45 @@
+/*
+ * Instead of reading each file sequentially, try to read all files at once, and concatenate them before printing the rle 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    int count = 0;
+    char chars[2] = "";
+    char prev[2] = "";
+
+    for (int c = 1; c < argc; c++) {
+        char* filename = strdup(argv[c]);
+        FILE* fd = fopen(filename, "r");
+        
+        if (fd == NULL) {
+            printf("wzip: cannot open file.\n");
+            exit(1);
+        }
+        while (fread(&chars, 1, 1, fd)) {
+            if (strcmp(chars, prev) == 0) {
+                count++;
+            } else {
+                if (prev[0] != '\0') {
+                    fwrite(&count, sizeof(int), 1, stdout);
+                    fwrite(prev, 1, 1, stdout);
+                }
+                count = 1;
+                strcpy(prev, chars);
+            }
+        }
+        fclose(fd);
+    }
+    
+    fwrite(&count, sizeof(int), 1, stdout);
+    fwrite(prev, 1, 1, stdout);
+
+    return 0;
+}
